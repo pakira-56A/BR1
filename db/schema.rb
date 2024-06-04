6dc48dc99effcf47feb9f0bc0eaf0ca10b795200
黒い子 <kanae@kuroikononotobukkukonpyuta.local>
2024-06-04 11:55:24 +0900

    ヘッダー/フッターのファイルを生成、編集。ルーティングは'tops#index'のまま

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_074434) do
+ActiveRecord::Schema[7.1].define(version: 20_240_529_074_434) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
