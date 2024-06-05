b31ba2a28361ba939014ed895c7d4a5a8833a3f9
黒い子 <kanae@kuroikononotobukkukonpyuta.local>
2024-06-05 15:18:16 +0900

    テキストを変更、重複したyieldを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # 背景オレンジのタスクス
 Rails.application.routes.draw do
-  resources :tasks
+  # 元のメモ　：　resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  # 元のメモ　：　get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
   # 　ここから編集
   # トップページに移動する指示
   root 'tops#index'
+  # 　ここから課題2のメモ
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
